5b681c0fd7533cc2de8900c4500f94da63a32086
Jilin <jilin@bu.edu>
2024-03-07 17:18:36 -0500

    reorganized code and repo, working on readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Jilin Zheng // U49258796
 
+## Scripts
+
+- [diyml.py](diyml.py): the module that ties all submodules together; also sets up the flask-restful app
+- [authentication.py](authentication.py): consists of the UserAPI, which supports all of the user-related CRUD functions
+- [database.py](database.py): connects the app to the MongoDB database
+
 ## Database Schemas - Going w/ MongoDB
 
 <img width="1015" alt="Screenshot 2024-02-29 104240" src="https://github.com/jilinzheng/ec530-DIYML/assets/133818802/29b13614-548a-46f1-8e84-107fd5cd2b4f">
--- authentication.py
+++ authentication.py
@@ -9,24 +9,24 @@ from database import users
 
 
 class UserAPI(Resource):
-    ''' API for all user-related functions '''
+    """ API for all user-related functions """
     def get(self, user_name):
-        ''' 
+        """ 
         Gets user information.
 
         :param user_name: 
         :returns: all fields associated with an user document
-        '''
+        """
         return json_encode(users.find_one({'user_name': user_name}))
 
     def post(self, user_name, user_pass):
-        ''' 
+        """ 
         Creates a new user.
         
         :param user_name: an unique user_name
         :param user_pass: the new user's associated password
         :returns: the new user's ObjectID
-        '''
+        """
         if users.find_one({'user_name': user_name}) is not None:
             return {'ERROR': 'user_name is NOT unique!'}
 
@@ -37,22 +37,22 @@ class UserAPI(Resource):
                                           {'_id': 1}))
 
     def put(self, user_name, user_pass):
-        '''
+        """
         Update the user_pass associated with the user_name.
 
         :param user_name: user whose password shall be updated
         :param new_pass: the new password
         :returns: the modified user information
-        '''
+        """
         users.update_one({'user_name': user_name},
                          {'$set': {'user_pass': user_pass}})
         return json_encode(users.find_one({'user_name': user_name}))
 
     def delete(self, user_name):
-        '''
+        """
         Delete a specified user.
         
         :param user_name: user to be deleted
-        '''
+        """
         users.delete_one({'user_name': user_name})
         return json_encode(users.find_one({'user_name': user_name}))
