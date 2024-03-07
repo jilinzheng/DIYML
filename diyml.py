aa39a78b94e8d0dfea192c8bb1b9d9ab76f44bf3
Jilin <jilin@bu.edu>
2024-03-06 23:48:48 -0500

    began implementing mongodb database

--- diyml.py
+++ diyml.py
@@ -1,33 +1,21 @@
 """
 Main application that uses all modules.
 """
-import os
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 # from auth import Users
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
 
-class Users(db.Model):
-    ''' Table to hold users, '''
-    uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), nullable=False, unique=True)
-    password = db.Column(db.String(200), nullable=False)
+client = MongoClient('localhost', 27017)
+db = client.diyml_db
+users = db.users
+images = db.images
+models = db.models
 
-    def __init__(self, uid, username, password):
-        self.uid = uid
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return self.username
-
-#api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
