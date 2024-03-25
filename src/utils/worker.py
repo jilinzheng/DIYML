fe3ea844b3283eaf35286b2897a988dd9ba62b2a
Jilin <jilin@bu.edu>
2024-03-24 20:15:11 -0400

    udpate worker

--- src/utils/worker.py
+++ src/utils/worker.py
@@ -1,15 +1,12 @@
-from __future__ import absolute_import
 import os
 import redis
 from rq import Worker, Queue, Connection
-import src.training
 
 listen = ['default']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    #with Connection(conn):
-        #worker = Worker(list(map(Queue, listen)))
-        worker = Worker(['default'],connection=conn)
+    with Connection(conn):
+        worker = Worker(list(map(Queue, listen)))
         worker.work()
