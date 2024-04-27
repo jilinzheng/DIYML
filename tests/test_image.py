136ecff664a604c157301d7c963cfd53134135ae
Jilin <jilin@bu.edu>
2024-04-27 01:17:10 -0400

    began to write swagger documentation and completed documenting /user route/module, also renamed testcases for simplicity

---
+++ tests/test_image.py
@@ -0,0 +1,56 @@
+"""
+Test valid and invalid CRUD operations on ImageUpload resource.
+"""
+
+
+import requests
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.utils.reset import reset
+
+
+user_url = 'http://127.0.0.1:5000/user'
+image_url = 'http://127.0.0.1:5000/image'
+files = {'file':open(os.path.join(os.path.dirname(__file__),
+                                  'test_image.png'),
+                                  'rb')}
+
+
+def test_init():
+    reset()
+
+
+def test_create_upload():
+    # first create a test user testName if it doesn't exist
+    params = {'user_name':'testName',
+              'user_pass':'testPass'}
+    response = requests.get(url=user_url,
+                            params=params)
+    if response.status_code == 400:
+        response = requests.post(url=user_url,
+                                 params=params)
+        assert response.status_code == 201 # created
+
+    # then test the image uploading with the testName user
+    params = {'user_name':'testName',
+              'category':'apple'}
+    response = requests.post(image_url,
+                             params=params,
+                             files=files)
+    assert response.status_code == 201 # created
+
+
+def test_delete_upload():
+    # delete the test image file
+    params = {'user_name':'testName',
+              'image_name':'test_image.png'}
+    response = requests.delete(url=image_url,
+                               params=params)
+    assert response.status_code == 202 # accepted
+
+    # also cleanup the test user created
+    params = {'user_name':'testName'}
+    response = requests.delete(user_url,
+                               params=params)
+    assert response.status_code == 202 # accepted
